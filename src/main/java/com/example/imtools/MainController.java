bad03c698d734bd268af5b1241c038315f10bb4b
robert1021 <robert.sparling196@gmail.com>
2023-05-21 00:36:01 -0400

    Added python interpreter

--- src/main/java/com/example/imtools/MainController.java
+++ src/main/java/com/example/imtools/MainController.java
@@ -5,14 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import java.io.*;
 import java.util.Objects;
 
 
@@ -39,6 +35,8 @@ public class MainController {
     TextField pdfPath;
     @FXML
     Pane appBar;
+    @FXML
+    private ProgressBar loadingBarPdfNpn;
 
     public void switchToRootScene(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
@@ -84,6 +82,50 @@ public class MainController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void runPythonTest() {
+
+        String text = pdfPath.getText();
+        updateProgress(0.10);
+        try {
+            String pythonInterpreterPath = "C:\\Users\\Robert\\Github-repo\\IM-Tools-App\\src\\main\\resources\\com\\example\\imtools\\python-interpreter\\python\\python.exe";
+            String pythonScriptPath = "C:\\Users\\Robert\\Github-repo\\IM-Tools-App\\src\\main\\resources\\com\\example\\imtools\\main.py";
+
+
+            ProcessBuilder processBuilder = new ProcessBuilder(pythonInterpreterPath, pythonScriptPath, text);
+            Process process = processBuilder.start();
+
+            // Read the output from the Python script
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Wait for the process to complete
+            int exitCode = process.waitFor();
+            System.out.println("Python script execution completed with exit code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        updateProgress(1.0);
+        // Show a message box
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Message");
+        alert.setHeaderText(null);
+        alert.setContentText("Complete");
+        // Retrieve the DialogPane and add a CSS file or inline styles
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(Objects.requireNonNull(getClass().getResource("main.css")).toExternalForm());
+        alert.showAndWait();
+        updateProgress(0.0);
+
+    }
+    public void updateProgress(double progress) {
+        loadingBarPdfNpn.setProgress(progress);
+    }
+
 
 
 }
---
+++ src/main/resources/com/example/imtools/main.py
@@ -0,0 +1,8 @@
+import sys
+
+arg1 = sys.argv[1]
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(f"Argument 1: {arg1}")
+
